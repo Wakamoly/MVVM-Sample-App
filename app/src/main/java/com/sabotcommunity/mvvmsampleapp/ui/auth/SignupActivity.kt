580f9724a18c53196fc76cf9923eb4c6b96f84b0
Wakamoly <lucidsoftworksllc@gmail.com>
2020-10-16 13:50:34 -0400

    Initial commit

---
+++ app/src/main/java/com/sabotcommunity/mvvmsampleapp/ui/auth/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.sabotcommunity.mvvmsampleapp.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sabotcommunity.mvvmsampleapp.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
